a54febc2f7ac43946b38bbd75950545d562c49b9
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-05-02 14:06:45 -0300

    Terminado a api com autenticação.

--- escola/models.py
+++ escola/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Aluno(models.Model):
+    objects = None
     nome = models.CharField(max_length=30)
     rg = models.CharField(max_length=9)
     cpf = models.CharField(max_length=11)
@@ -9,6 +10,7 @@ class Aluno(models.Model):
     def __str__(self):
         return self.nome
 class Curso(models.Model):
+    objects = None
     NIVEL = (
         ('B', 'Básico'),
         ('I', 'Intermediário'),
@@ -21,3 +23,13 @@ class Curso(models.Model):
 
     def __str__(self):
         return self.descricao
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno')
+    )
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    periodo = models.CharField(max_length=1, choices=PERIODO, blank=False, null=False,default='M')
